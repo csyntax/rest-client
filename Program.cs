76b2663b97e685cda7b573b152777aae7a8be8cf
csyntax <csyntax@outlook.com>
2016-04-28 21:30:37 +0300

    add result in task

--- Program.cs
+++ Program.cs
@@ -1,38 +1,17 @@
 ﻿using System;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Collections.Generic;
-using System.Runtime.Serialization.Json;
 
 namespace RestClient
 {
     public class Program
     {
-        private static async Task ProcessRepositories()
-        {
-            var client = new HttpClient();
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-                client.DefaultRequestHeaders.Add("User-Agent", "csyntax RestClient");
-
-                var stringTask = client.GetStringAsync("https://api.github.com/users/csyntax/repos");
-                var msg = await stringTask;
-                
-                var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
-                
-                var streamTask = client.GetStreamAsync("https://api.github.com/users/csyntax/repos");
-                var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
-                
-                foreach (var repository in repositories)
-                {
-                    Console.WriteLine(repository.Name);
-                }
-        }
-
         public static void Main(string[] args)
         {
-            ProcessRepositories().Wait();
+            var repositories = Repository.ProcessRepositories().Result;
+
+            foreach (var repository in repositories) 
+            {
+                Console.WriteLine(repository.Name);
+            }      
         }
     }
 }
--- Repository.cs
+++ Repository.cs
@@ -1,12 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace RestClient
 {
-    
     [DataContract(Name="repo")]
     public class Repository
     {
         [DataMember(Name="name")]
-        public string Name { get; set; }
+        public string Name { get; private set; }
+        public static async Task<List<Repository>> ProcessRepositories()
+        {
+            var client = new HttpClient();
+            
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "csyntax");
+                
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));                
+            var streamTask = client.GetStreamAsync("https://api.github.com/users/csyntax/repos");
+            /*var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+                
+            foreach (var repository in repositories)
+            {
+                Console.WriteLine(repository.Name);
+            }*/
+            
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+            
+            return repositories;
+        }
     }
 }
