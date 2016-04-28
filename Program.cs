bf0c88632be0b89ca02bed3390867659611529ca
csyntax <csyntax@outlook.com>
2016-04-28 10:42:54 +0300

    add async task for get api

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
-namespace ConsoleApplication
+namespace RestClient
 {
     public class Program
     {
+        private static async Task ProcessRepositories()
+        {
+            var client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                client.DefaultRequestHeaders.Add("User-Agent", "csyntax RestClient");
+
+                var stringTask = client.GetStringAsync("https://api.github.com/users/csyntax/repos");
+                var msg = await stringTask;
+                
+                Console.Write(msg);
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProcessRepositories().Wait();
         }
     }
 }
